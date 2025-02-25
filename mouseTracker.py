525bc9e0cc34a425fc6801155afe7468e520aa39
tokimay <tokimay@gmail.com>
2025-02-25 13:08:01 +0330

    License 403.12

--- mouseTracker.py
+++ mouseTracker.py
@@ -1,3 +1,22 @@
+
+# This file is part of https://github.com/tokimay/random_entropy
+# Copyright (C) 2016 https://github.com/tokimay
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# This software is licensed under GPLv3. If you use or modify this project,
+# you must include a reference to the original repository: https://github.com/tokimay/random_entropy
+
 import sys
 from PySide6.QtWidgets import QApplication
 from PySide6 import QtWidgets
